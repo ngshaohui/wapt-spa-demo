76bc4e9b1465a9a039a416ae7ef7b0e78c07b793
shaohui <shinlos@hotmail.com>
2023-10-07 02:34:39 +0800

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,43 @@
+'use client';
+
+import { Alignment, Button, Navbar, Switch } from '@blueprintjs/core';
 import './globals.css';
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { useState } from 'react';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'Single Page Application',
-  description: 'Learning about embedded API calls',
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [darkTheme, setDarkTheme] = useState(false);
+  function handleSwitch() {
+    setDarkTheme((prevTheme) => !prevTheme);
+  }
+  const cssClassName = `${inter.className} ${darkTheme ? 'bp5-dark' : ''}`;
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cssClassName}>
+        <Navbar>
+          <Navbar.Group align={Alignment.LEFT}>
+            <Navbar.Heading>SPA Demo</Navbar.Heading>
+            <Navbar.Divider />
+            <Link href="/">
+              <Button className="bp5-minimal" icon="home" text="Home" />
+            </Link>
+            <Link href="/dashboard">
+              <Button className="bp5-minimal" icon="dashboard" text="Dashboard" />
+            </Link>
+          </Navbar.Group>
+          <Navbar.Group align={Alignment.RIGHT}>
+            <Switch
+              onClick={handleSwitch}
+              alignIndicator={Alignment.RIGHT}
+              labelElement={'Dark Mode'}
+            />
+          </Navbar.Group>
+        </Navbar>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,17 @@
+import { Menu, MenuItem } from '@blueprintjs/core';
+import styles from './Sidebar.module.css';
+
+export default function Sidebar() {
+  function getStatistics() {
+    fetch('/api/v1/statistics');
+  }
+  function makePost() {
+    fetch('/api/v2/post', { method: 'POST' });
+  }
+  return (
+    <Menu className={styles.sidebar}>
+      <MenuItem onClick={makePost} icon="new-text-box" text="New post" />
+      <MenuItem onClick={getStatistics} icon="refresh" text="Get statistics" />
+    </Menu>
+  );
+}
