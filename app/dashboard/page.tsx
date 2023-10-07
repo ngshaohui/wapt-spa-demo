73d2e851a09bdb27859c53e9f57638d11e89ef1d
shaohui <shinlos@hotmail.com>
2023-10-07 12:26:04 +0800

    wip

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,8 @@ import { useEffect } from 'react';
 export default function Dashboard() {
   useEffect(() => {
     fetch('/api/v2/profile');
+    fetch('/api/v2/posts');
+    fetch('/api/v2/statistics');
   }, []);
-  return <div></div>;
+  return <div>Dashboard</div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import Sidebar from '@/components/Sidebar/Sidebar';
 import styles from './page.module.css';
 import { useEffect } from 'react';
+import { Card, HTMLTable } from '@blueprintjs/core';
 
 export default function Home() {
   useEffect(() => {
@@ -12,6 +13,38 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Sidebar />
+      <Card className={styles.card}>
+        <HTMLTable>
+          <thead>
+            <tr>
+              <th>Project</th>
+              <th>Description</th>
+              <th>Technologies</th>
+              <th>Contributors</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Blueprint</td>
+              <td>CSS framework and UI toolkit</td>
+              <td>Sass, TypeScript, React</td>
+              <td>268</td>
+            </tr>
+            <tr>
+              <td>TSLint</td>
+              <td>Static analysis linter for TypeScript</td>
+              <td>TypeScript</td>
+              <td>403</td>
+            </tr>
+            <tr>
+              <td>Plottable</td>
+              <td>Composable charting library built on top of D3</td>
+              <td>SVG, TypeScript, D3</td>
+              <td>737</td>
+            </tr>
+          </tbody>
+        </HTMLTable>
+      </Card>
     </main>
   );
 }
